786e02da202b3f1ab1f6151e8a544bb7f9d040be
danewest <cameron.west676@topper.wku.edu>
2025-04-02 18:43:52 -0500

    Edited Lexical Analyzer

--- Lexical Analyzer.c
+++ Lexical Analyzer.c
@@ -1,8 +1,43 @@
 #include<stdio.h>
+#include<ctype.h>
 #include<string.h>
 
-int main() {
-    char string[] = "#lol!";
+/* Global declarations for variables and functions */
 
-    printf("%s\n", string);
+// Variables
+int charClass;
+char lexeme[100];
+char nextChar;
+int lexLen;
+int token;
+int nextToken;
+FILE *in_fp, *fopen();
+
+// Functions
+void addChar();
+void getChar();
+void getNonBlank();
+int lex();
+
+// Classes
+#define LETTER 0
+#define DIGIT 1
+#define UNKNOWN 99
+
+// Token Codes
+#define INT_LIT 10
+#define IDENT 11
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
+
+/**********************************************************************************************/
+/* main driver */
+main() {
+    /* Open the input data file and process its contents */
+    if ((in_fp))
 }
